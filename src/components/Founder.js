59873f2a251f3e1ec2f0671dec8abfc2b680b606
Prakash Mahat <shadyprakash8@gmail.com>
2021-07-13 10:16:14 +0545

    founder section responsive issue fixed on screen 375

--- src/components/Founder.js
+++ src/components/Founder.js
@@ -2,8 +2,11 @@
 
 import React, {Fragment, useState} from 'react';
 import {Container, Row, Col} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 import Founder1 from '../assets/uploads/Founders/Image.jpg';
 import Founder2 from '../assets/uploads/Image.jpg';
+import RightArrow from '../assets/uploads/Hero/XL/Button/Icon/Arrow/Right-white.svg';
+
 const Founder = () => {
 	const [founder, setFounder] = useState(Founder1);
 
@@ -15,13 +18,43 @@ const Founder = () => {
 						<Col sm={12} md={12}>
 							<h5 className='text-uppercase text-white'>The founders</h5>
 						</Col>
+						<Col sm={12} className='d-xl-none'>
+							<div className='founder-content'>
+								<figure className='mb-3'>
+									<img src={founder} alt='founder image...'></img>
+								</figure>
+								<div className='founder-content--name'>
+									<h2 className='text-white'>
+										<Link to='/'>
+											Maria Kenter&nbsp;&nbsp;{' '}
+											<img src={RightArrow} alt='right arrow'></img>
+										</Link>
+									</h2>
+								</div>
+							</div>
+							<div className='founder-content'>
+								<figure>
+									<img src={Founder2} alt='founder image...'></img>
+								</figure>
+								<div className='founder-content--name'>
+									<h2 className='text-white'>
+										<Link to='/'>
+											Maria Kenter&nbsp;&nbsp;{' '}
+											<img src={RightArrow} alt='right arrow'></img>
+										</Link>
+									</h2>
+								</div>
+							</div>
+						</Col>
 					</Row>
 				</Container>
-				<div className='founder__bg'>
+
+				<div className='founder__bg d-xs-none'>
 					<figure>
 						<img src={founder} alt='founder image...'></img>
 					</figure>
-					<div className='founder__bg--content'>
+
+					<div className='founder__bg--content '>
 						<ul>
 							<li>
 								<h2 className='text-white' onClick={() => setFounder(Founder1)}>
