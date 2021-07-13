f4aa98de43a4cad723b8a31d85942c89514f05c1
Prakash Mahat <shadyprakash8@gmail.com>
2021-07-13 07:42:32 +0545

    latest news slider issues fixed on screen 375

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -9,13 +9,33 @@ import Image1 from '../../assets/uploads/Tiles/Tile/Tile1.jpg';
 import Image2 from '../../assets/uploads/Tiles/Tile/Tile2.jpg';
 import Image3 from '../../assets/uploads/Tiles/Tile/Tile3.jpg';
 import Image4 from '../../assets/uploads/Tiles/Tile/Tile4.jpg';
-
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 const News = () => {
+	const responsive = {
+		desktop: {
+			breakpoint: {max: 3000, min: 1024},
+			items: 3,
+			slidesToSlide: 3, // optional, default to 1.
+		},
+		tablet: {
+			breakpoint: {max: 1024, min: 464},
+			items: 2,
+			slidesToSlide: 2, // optional, default to 1.
+		},
+		mobile: {
+			breakpoint: {max: 464, min: 0},
+			items: 1,
+			slidesToSlide: 1,
+			partialVisibilityGutter: 60, // optional, default to 1.
+		},
+	};
 	return (
 		<Fragment>
 			<section className='latestNews'>
+				{/* Desktop screen */}
 				<Container>
-					<Row className='d-xl-flex d-xs-none'>
+					<Row className='d-xs-none d-xl-flex '>
 						<Col>
 							<div className='latestNews__left'>
 								<Row>
@@ -74,56 +94,55 @@ const News = () => {
 						</Col>
 					</Row>
 				</Container>
-				<div className='latestNews-wrapper'>
-					<Card
-						type='newsCard__lg'
-						image={Image1}
-						title='Molestie molesti metus, tempus in commodo, semper tellus'
-						subtitle=''
-						text=''
-					/>
-					<Card
-						type='newsCard__lg'
-						image={Image1}
-						title='Molestie molesti metus, tempus in commodo, semper tellus'
-						subtitle=''
-						text=''
-					/>
-					<Card
-						type='newsCard__lg'
-						image={Image1}
-						title='Molestie molesti metus, tempus in commodo, semper tellus'
-						subtitle=''
-						text=''
-					/>
-					<Card
-						type='newsCard__lg'
-						image={Image1}
-						title='Molestie molesti metus, tempus in commodo, semper tellus'
-						subtitle=''
-						text=''
-					/>
-					<Card
-						type='newsCard__lg'
-						image={Image1}
-						title='Molestie molesti metus, tempus in commodo, semper tellus'
-						subtitle=''
-						text=''
-					/>
-					<Card
-						type='newsCard__lg'
-						image={Image1}
-						title='Molestie molesti metus, tempus in commodo, semper tellus'
-						subtitle=''
-						text=''
-					/>
-					<Card
-						type='newsCard__sm'
-						image={Image4}
-						title=''
-						subtitle=''
-						text='In faucibus suspendisse magna massa nibh elementum augue phasellus tristique senectus urna'
-					/>
+
+				{/* Mobile screen */}
+				<div className='latestNews-slider d-xl-none'>
+					<h5 className='text-uppercase color-brand'>latest news</h5>
+					<Carousel
+						swipeable={true}
+						draggable={true}
+						showDots={true}
+						responsive={responsive}
+						infinite={true}
+						autoPlaySpeed={1000}
+						partialVisible
+						keyBoardControl={true}
+						dotListClass=''
+						customTransition='all .5'
+						transitionDuration={500}
+						containerClass='carousel-container d-xl-none'
+						removeArrowOnDeviceType={['desktop', 'tablet', 'mobile']}
+						itemClass='carousel-item-padding-40-px'>
+						<Card
+							type='newsCard__lg'
+							image={Image1}
+							title=''
+							subtitle='Molestie molesti metus, tempus in commodo, semper tellus'
+							text=''
+						/>
+						<Card
+							type='newsCard__lg'
+							image={Image1}
+							title=''
+							subtitle='Molestie molesti metus, tempus in commodo, semper tellus'
+							text=''
+						/>
+						<Card
+							type='newsCard__lg'
+							image={Image1}
+							title=''
+							subtitle='Molestie molesti metus, tempus in commodo, semper tellus'
+							text=''
+						/>
+						<Card
+							type='newsCard__lg'
+							image={Image1}
+							title=''
+							subtitle='Molestie molesti metus, tempus in commodo, semper tellus'
+							text=''
+						/>
+					</Carousel>
+					<Button type='btn__secondary' text='See all news' />
 				</div>
 			</section>
 		</Fragment>
